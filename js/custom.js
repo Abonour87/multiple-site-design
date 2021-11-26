23739a0d7d4b6fde62b8291cbf5e9e0a08b2c79f
myname <myemail>
2021-11-26 10:01:58 +0200

    first multiple page

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(function() {
+    $(".home .nava .small-but").click(function() {
+        $(".home .nava .video-link ul").slideToggle()
+    })
+})
+// -------------page about.html--------------
+$(function() {
+    $(".home2 .nava .small-but").click(function() {
+        $(".home2 .nava .video-link ul").slideToggle()
+    })
+})
+// -----------------page contact.html-----------
+$(function() {
+    $(".home3 .nava .small-but").click(function() {
+        $(".home3 .nava .video-link ul").slideToggle()
+    })
+})
